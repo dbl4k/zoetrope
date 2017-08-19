69994f5ccb11ad3502e8b2c01f9bf7be04a0ae10
dbl4k <github@dbl4k.net>
2017-08-19 18:07:19 +0100

    add manager viewmodel

---
+++ Zoetrope/ViewModels/Manager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zoetrope.ViewModels
+{
+    class Manager : INotifyPropertyChanged
+    {
+
+        public string NewWidgetPath { get; set; } = "https://m.popkey.co/7c2979/E8pyv_s-200x150.gif";
+
+        // INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
