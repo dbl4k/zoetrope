de97741f3ecec0a0dad651fc2a41bd476e353504
dbl4k <github@dbl4k.net>
2017-08-19 18:50:28 +0100

    add widget factory

---
+++ Zoetrope/Factories/WidgetFactory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zoetrope.Factories
+{
+    class WidgetFactory
+    {
+        public static Widgets.Widget Create(string path)
+        {
+            Widgets.Widget widget = new Widgets.Widget();
+            widget.SetAnimatedImage(path);
+            return widget;
+        }
+
+    }
+}
