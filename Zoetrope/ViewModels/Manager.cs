09a2450be22d26b869d3a2e1cd45bfb1ac718cfc
dbl4k <github@dbl4k.net>
2017-08-20 11:02:26 +0100

    add some initial inotifypropertychanged implementation

--- Zoetrope/ViewModels/Manager.cs
+++ Zoetrope/ViewModels/Manager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -16,5 +17,13 @@ namespace Zoetrope.ViewModels
 
         // INotifyPropertyChanged
         public event PropertyChangedEventHandler PropertyChanged;
+        
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
     }
 }
